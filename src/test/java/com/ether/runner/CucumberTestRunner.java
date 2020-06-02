3f040b930688b766d52b3f30065643caafd5b2c9
Syed Naser Uddin <syednaseru@maveric-systems.com>
2020-06-02 18:05:46 +0530

    updated cucumber annotation

--- src/test/java/com/ether/runner/CucumberTestRunner.java
+++ src/test/java/com/ether/runner/CucumberTestRunner.java
@@ -15,7 +15,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
         features = {"src/test/java/Feature_Files/"},
         glue = "com.ether.steps",
         dryRun=false,
-        tags = {"@Test"}
+        tags = {"@Syed"}
         )
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
